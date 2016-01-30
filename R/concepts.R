64ba23767ab3e1ce3ec4b6c31e4a1412b67f1005
mpancia <msp@themsp.org>
2016-01-30 10:01:18 -0800

    Added concept definition searching.

--- R/concepts.R
+++ R/concepts.R
@@ -2,8 +2,6 @@
 #
 #'
 #' @param CUI
-#' @param pageNumber
-#' @param pageSize
 #'
 #' @return
 #' @export
@@ -20,3 +18,23 @@ get_concept_rels_page <- function(CUI, pageNumber = 1, pageSize = 25 ){
   r
 }
 
+#' Get UMLS concept definitions.
+#
+#'
+#' @param CUI CUI of interest.
+#' @param sabs Source vocabularies, comma separated.
+#'
+#' @return
+#' @export
+#'
+get_concept_defs <- function(CUI, sabs = NULL)
+{
+  exhaust_search(FUN = get_concept_defs_page, CUI = CUI, sabs = sabs)
+}
+
+#' @rdname get_concept_defs
+get_concept_defs_page <- function(CUI, sabs = NULL, pageNumber = 1, pageSize = 25 ){
+  params = list(ticket = get_service_ticket(get_TGT()), sabs = sabs, pageNumber = pageNumber, pageSize = pageSize)
+  r <- GET(restBaseURL, path = paste0("rest/content/current/CUI/", CUI, "/definitions"), query = params)
+  r
+}
