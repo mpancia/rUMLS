73a0d3f7e6699c577bdb23a01b7abb6b7e36ad52
mpancia <msp@themsp.org>
2016-01-30 10:10:18 -0800

    Completed concept search functions.

--- R/concepts.R
+++ R/concepts.R
@@ -38,3 +38,51 @@ get_concept_defs_page <- function(CUI, sabs = NULL, pageNumber = 1, pageSize = 2
   r <- GET(restBaseURL, path = paste0("rest/content/current/CUI/", CUI, "/definitions"), query = params)
   r
 }
+
+#' Get UMLS concept atoms.
+#
+#'
+#' @param CUI CUI of interest.
+#' @param sabs Source vocabularies, comma separated.
+#' @param ttys
+#' @param language
+#' @return
+#' @export
+#'
+get_concept_atoms <- function(CUI, sabs = NULL, ttys = NULL, language = NULL, includeObsolete = FALSE, includeSuppressible = FALSE)
+{
+  exhaust_search(FUN = get_concept_atoms_page, CUI = CUI, sabs = sabs, ttys = ttys, language = language, includeObsolete = includeObsolete,
+                  includeSuppressible = includeSuppressible)
+}
+
+#' @rdname get_concept_atoms
+get_concept_atoms_page <- function(CUI, sabs = NULL, ttys = NULL, language = NULL, includeObsolete = FALSE, includeSuppressible = FALSE,
+                          pageNumber = 1, pageSize = 25 )
+{
+  params = list(ticket = get_service_ticket(get_TGT()), sabs = sabs, pageNumber = pageNumber, pageSize = pageSize)
+  r <- GET(restBaseURL, path = paste0("rest/content/current/CUI/", CUI, "/atoms"), query = params)
+  r
+}
+
+#' Get UMLS concept info
+#
+#'
+#' @param CUI CUI of interest.
+#' @param sabs Source vocabularies, comma separated.
+#' @param ttys
+#' @param language
+#' @return
+#' @export
+#'
+get_concept_info <- function(CUI)
+{
+  exhaust_search(FUN = get_concept_defs_page, CUI = CUI )
+}
+
+#' @rdname get_concept_info
+get_concept_info_page <- function(CUI, pageNumber = 1, pageSize = 25 )
+{
+  params = list(ticket = get_service_ticket(get_TGT()), pageNumber = pageNumber, pageSize = pageSize)
+  r <- GET(restBaseURL, path = paste0("rest/content/current/CUI/", CUI, "/atoms"), query = params)
+  r
+}
